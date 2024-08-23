86b9f44afa50b7d36446561709043a26d8aae2e8
joseaam2020 <joseaam2019@hotmail.com>
2024-08-22 20:53:49 -0600

    Modificaciones aluPara, aun sin flags y no funcional

--- labDOS/aluPara.sv
+++ labDOS/aluPara.sv
@@ -1,42 +1,76 @@
-module aluP (
+module aluPara #(parameter N = 4) (	
 	
-	
-	logic [3:0] a;
-	logic [3:0] b;
-	
+	input logic [N-1:0] a;
+	input logic [N-1:0] b;	
 	input logic selector
-	
-	output logic s_restador,cout_restador
-	output logic s_sumador,cout_rsumador
-	output logic s_multiplicador
-	output logic s_division
-	
-	output logic shift_l
-	output logic shift_r
-	output logic s_and
-	output logic s_or
-	output logic s_xor
-	
-	output logic shift_r
-	output logic shift_r
-	
-	output logic f_C
-	output logic f_N
-	output logic f_Z
-	output logic f_V
-	
 
+	output logic [N-1:0] resultado;
+	output logic [6:0] display_selector1;
+	output logic [6:0] display_selector2;
 )
 
-
-	mux16to1 implementado(
+	//Se define Contador
+	logic reset_selector;
+	logic we_selecetor;
+	logic [N-1:0] wq_selector; //write q 
+	logic [N-1:0] q_selector;
 	
-        .s(s),
-        .in(selector),  
-        .out(out) // no sé si se deba buscar tomar todas las salidas 
-		  
-		  
+	restPara #(N) contador(
+		.clk(selector),
+		.reset(reset_selector),
+		.we(we_selecetor)
+		.wq(wq_selector),
+		.q(q_selector),
+		.display1(display_selector1),
+		.display2(display_selector2)
+	); 
+
+	//Se define Mux 
+    input logic [15:0][N-1:0] mux_in;
+	
+	mux16to1 #(N) nuevo_mux (	
+        .s(q_selector),
+        .in(mux_in),  
+        .out(resultado) 
     );
+
+	//Se define sumador	
+	Sumador_estructural #(N) nuevo_sumandor (
+		.a(a),
+		.b(b),
+		.cin(0),
+		.cout_sumador(), //Hay que pegarlo a flag de carry 
+		.s_sumador(mux_in[0]),
+	);
+
+	//Se define restador
+	restador #(N) nuevo_restador (
+		.a(a),
+		.b(b),
+		.cin(0),
+		.cout_restador(), //Hay que pegarlo a flag de carry 
+		.s_restador(mux_in[1]),
+	); 
+
+	//Se define multiplicador
+	logic [(2*N)-1:0] resulto_multiplicador; 
+	
+	multiplicador #(N) nuevo_multiplicador(
+		.a(a),
+		.b(b),
+		.s_multiplicador(resulto_multiplicador),
+	);
+
+	assign mux_in[2] = resulto_multiplicador[N-1:0];
+
+	//Se definen los resultados de las otras operaciones
+	assign mux_in[3] = a / b;
+	assign mux_in[4] = a % b;
+	assign mux_in[5] = a and b;
+	assign mux_in[6] = a or b;
+	assign mux_in[7] = a xor b; 
+	assign mux_in[8] = a << 1;
+	assign mux_in[9] = a >> 1;
 
 	
 endmodule	
---
+++ labDOS/restPara.sv
@@ -0,0 +1,35 @@
+
+module restPara #(parameter N = 6)(clk,reset,q,wq,we,display1,display2);
+
+	input logic clk;
+	input logic reset;
+	input logic we; //write enable
+	input logic [N-1:0] wq; //write q 
+	output logic [N-1:0] q;
+	output logic [6:0] display1;
+	output logic [6:0] display2;
+	
+	logic [3:0] bin_in1;
+	logic [3:0] bin_in2;
+	
+	always_ff @(posedge clk, posedge reset, posedge we) begin
+	
+		if (reset) q <= 0;
+		
+		else if (we) begin
+		
+			for (int i = 0; i < N; i++) q[i] <= wq[i];
+			
+		end else q <= q - 1;
+		
+	
+	end
+		
+	assign bin_in1 = q / 10;
+	assign bin_in2 = q % 10;
+		
+	SevenSegmentDisplay seven1 (.binary_input(bin_in1), .display_output(display1));
+	SevenSegmentDisplay seven2 (.binary_input(bin_in2), .display_output(display2));
+
+endmodule
+
